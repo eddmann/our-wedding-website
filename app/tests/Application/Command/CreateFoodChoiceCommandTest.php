400572f760074d841b953204567481802e7c8ee6
Edd Mann <the@eddmann.com>
2021-11-20 15:21:50 +0000

    Fix `CreateFoodChoiceCommandTest` namespace

--- app/tests/Application/Command/CreateFoodChoiceCommandTest.php
+++ app/tests/Application/Command/CreateFoodChoiceCommandTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Tests\Application;
+namespace App\Tests\Application\Command;
 
 use App\Application\Command\CreateFoodChoice\{CreateFoodChoiceCommand, CreateFoodChoiceCommandHandler};
 use App\Domain\Model\FoodChoice\{FoodChoiceName, FoodChoiceRepository, FoodCourse};
