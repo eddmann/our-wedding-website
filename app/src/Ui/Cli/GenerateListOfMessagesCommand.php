4e2ecef471fe83a3f607dd515e5c2a07704ceae8
Edd Mann <the@eddmann.com>
2021-11-23 00:31:59 +0000

    Add message listing documentation command

---
+++ app/src/Ui/Cli/GenerateListOfMessagesCommand.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+
+namespace App\Ui\Cli;
+
+use App\Application\Command\Command as ApplicationCommand;
+use App\Domain\Helpers\{AggregateEvent, DomainEvent};
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class GenerateListOfMessagesCommand extends Command
+{
+    private const MESSAGE_TYPES = [
+        ApplicationCommand::class,
+        AggregateEvent::class,
+        DomainEvent::class,
+    ];
+    public static $defaultName = 'documentation:message-list';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('All Messages');
+
+        $this->requireAllSrc();
+        $declaredClasses = \get_declared_classes();
+        \sort($declaredClasses);
+
+        foreach (self::MESSAGE_TYPES as $messageType) {
+            $io->section('Message Type: ' . $this->getClassName($messageType));
+
+            $table = new Table($output);
+            $table->setHeaders(['Name', 'Attributes']);
+
+            foreach ($declaredClasses as $fqcnClassName) {
+                if (\in_array($messageType, \class_implements($fqcnClassName), true)) {
+                    $table->addRow([
+                        $this->getClassName($fqcnClassName),
+                        $this->getMessageAttributes($fqcnClassName),
+                    ]);
+                }
+            }
+
+            $table->render();
+            $io->newLine();
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function requireAllSrc(): void
+    {
+        $srcPath = \realpath(__DIR__ . '/../../');
+
+        $phpFiles = new \RegexIterator(
+            new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($srcPath)),
+            '/\.php$/'
+        );
+
+        foreach ($phpFiles as $phpFile) {
+            require_once $phpFile;
+        }
+    }
+
+    private function getClassName(string $fqcnClassName): string
+    {
+        $separated = \explode('\\', $fqcnClassName);
+
+        return $separated[\count($separated) - 1];
+    }
+
+    private function getMessageAttributes(string $fqcnClassName): string
+    {
+        $attributes = \array_map(
+            static fn (\ReflectionParameter $parameter) => $parameter->getName(),
+            (new \ReflectionClass($fqcnClassName))->getConstructor()->getParameters()
+        );
+
+        return \implode(', ', $attributes);
+    }
+}
