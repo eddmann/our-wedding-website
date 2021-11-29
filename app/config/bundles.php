1e8df1c1e5351565e519732a2edb5d221129bf08
Edd Mann <the@eddmann.com>
2021-11-29 17:37:31 +0000

    Add Symfony security bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Bref\Symfony\Messenger\BrefMessengerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
