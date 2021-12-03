cf1e1233be64ecd5c38cfc8ec746303121567e63
Edd Mann <the@eddmann.com>
2021-12-03 23:20:22 +0000

    Add Symfony profiler

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Bref\Symfony\Messenger\BrefMessengerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
