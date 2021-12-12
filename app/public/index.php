dc65a2fc4915ca41913f45a67697be65cbfb6bde
Edd Mann <the@eddmann.com>
2021-12-12 20:05:12 +0000

    Add proxy auth key check to front-controller

--- app/public/index.php
+++ app/public/index.php
@@ -2,6 +2,12 @@
 
 use App\Infrastructure\Kernel;
 
+if (($proxyAuthKeyHeader = \getenv('PROXY_AUTH_KEY_HEADER'))
+    && \getenv('PROXY_AUTH_KEY_VALUE') !== ($_SERVER[$proxyAuthKeyHeader] ?? '')) {
+    \http_response_code(401);
+    exit();
+}
+
 require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return static fn (array $context) => new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
