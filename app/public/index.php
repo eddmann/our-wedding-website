3864b2ecb58c27319f63708cba946ac1e1f5cb52
Edd Mann <the@eddmann.com>
2021-12-23 15:24:02 +0000

    Fix front controller auth. header guard

--- app/public/index.php
+++ app/public/index.php
@@ -2,10 +2,20 @@
 
 use App\Infrastructure\Kernel;
 
-if (($proxyAuthKeyHeader = \getenv('PROXY_AUTH_KEY_HEADER'))
-    && \getenv('PROXY_AUTH_KEY_VALUE') !== ($_SERVER["HTTP_{$proxyAuthKeyHeader}"] ?? '')) {
-    \http_response_code(401);
-    exit();
+if ($_ENV['PROXY_AUTH_KEY_HEADER'] ?? false) {
+    $proxyAuthKeyHeader = 'HTTP_' . \str_replace('-', '_', \mb_strtoupper($_ENV['PROXY_AUTH_KEY_HEADER']));
+    if ($_ENV['PROXY_AUTH_KEY_VALUE'] !== ($_SERVER[$proxyAuthKeyHeader] ?? '')) {
+        \http_response_code(401);
+        exit();
+    }
+}
+
+if ($_ENV['HTTPS'] ?? false) {
+    $_SERVER['HTTPS'] = $_ENV['HTTPS'];
+}
+
+if ($_ENV['HOST'] ?? false) {
+    $_SERVER['HTTP_HOST'] = $_ENV['HOST'];
 }
 
 require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
