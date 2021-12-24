a2dc1ed9f7a4f10e6ad17b865e67844315c71bbd
Edd Mann <the@eddmann.com>
2021-12-24 17:00:43 +0000

    Add example mailer usage

--- app/src/Infrastructure/DomainEventSubscribers/InviteCreatedAuditLogger.php
+++ app/src/Infrastructure/DomainEventSubscribers/InviteCreatedAuditLogger.php
@@ -5,15 +5,30 @@ namespace App\Infrastructure\DomainEventSubscribers;
 use App\Domain\Event\InviteCreated;
 use App\Domain\Helpers\DomainEventSubscriber;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 final class InviteCreatedAuditLogger implements DomainEventSubscriber
 {
-    public function __construct(private LoggerInterface $infoLogger)
-    {
+    /** @param string[] $emailNotifierTo */
+    public function __construct(
+        private LoggerInterface $infoLogger,
+        private MailerInterface $mailer,
+        private array $emailNotifierTo,
+        private string $emailNotifierFrom
+    ) {
     }
 
     public function __invoke(InviteCreated $event): void
     {
         $this->infoLogger->info('Invite was created', ['id' => $event->id]);
+
+        $email = (new Email())
+            ->to(...$this->emailNotifierTo)
+            ->from($this->emailNotifierFrom)
+            ->subject('An invite was created')
+            ->text("An invite with id {$event->id} was created");
+
+        $this->mailer->send($email);
     }
 }
