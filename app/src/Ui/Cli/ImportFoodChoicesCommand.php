a5d8d88c8babb0b6a8376fca43e92497ec0cc7fd
Edd Mann <the@eddmann.com>
2022-01-23 15:01:26 +0000

    Add food choice/invite import commands

---
+++ app/src/Ui/Cli/ImportFoodChoicesCommand.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace App\Ui\Cli;
+
+use App\Application\Command\CommandBus;
+use App\Application\Command\CreateFoodChoice\CreateFoodChoiceCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class ImportFoodChoicesCommand extends Command
+{
+    private CommandBus $bus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        parent::__construct('app:import-food-choices');
+
+        $this->addArgument('file', InputArgument::REQUIRED);
+
+        $this->bus = $commandBus;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $file = $input->getArgument('file');
+
+        $choices = \json_decode_array(\file_get_contents($file));
+
+        $output->writeln(
+            \sprintf(
+                'Importing <info>%s</info> food choices from <info>%s</info>',
+                \count($choices),
+                $file
+            )
+        );
+
+        foreach ($choices as $choice) {
+            $this->bus->dispatch(
+                new CreateFoodChoiceCommand($choice['guestType'], $choice['course'], $choice['name'])
+            );
+        }
+
+        return 0;
+    }
+}
---
+++ app/src/Ui/Cli/ImportInvitesCommand.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace App\Ui\Cli;
+
+use App\Application\Command\CommandBus;
+use App\Application\Command\CreateInvite\CreateInviteCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class ImportInvitesCommand extends Command
+{
+    private CommandBus $bus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        parent::__construct('app:import-invites');
+
+        $this->addArgument('file', InputArgument::REQUIRED);
+
+        $this->bus = $commandBus;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $file = $input->getArgument('file');
+
+        $invites = \json_decode_array(\file_get_contents($file));
+
+        $output->writeln(
+            \sprintf(
+                'Importing <info>%s</info> invites from <info>%s</info>',
+                \count($invites),
+                $file
+            )
+        );
+
+        foreach ($invites as $invite) {
+            $this->bus->dispatch(new CreateInviteCommand($invite['type'], $invite['guests']));
+        }
+
+        return 0;
+    }
+}
