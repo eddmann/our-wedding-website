43f66726f9bc9d39d7146ba3187b8c7bacff5fcd
Edd Mann <the@eddmann.com>
2022-03-29 07:51:39 +0100

    Store `EventStreamPointer` as string
    This switches to store the internal event stream pointer as a string, so as to be used in the future for different backend implementations.

--- app/src/Domain/Helpers/EventStreamPointer.php
+++ app/src/Domain/Helpers/EventStreamPointer.php
@@ -5,23 +5,33 @@ namespace App\Domain\Helpers;
 /** @psalm-immutable */
 final class EventStreamPointer
 {
-    private function __construct(private int $position)
+    private function __construct(private string $position)
     {
     }
 
-    public static function fromInt(int $position): self
+    public static function fromString(string $position): self
     {
         return new self($position);
     }
 
+    public static function fromInt(int $position): self
+    {
+        return new self((string) $position);
+    }
+
     public static function beginning(): self
     {
-        return new self(0);
+        return new self('');
+    }
+
+    public function toString(): string
+    {
+        return $this->position;
     }
 
     public function toInt(): int
     {
-        return $this->position;
+        return (int) $this->position;
     }
 
     public function equals(self $that): bool
