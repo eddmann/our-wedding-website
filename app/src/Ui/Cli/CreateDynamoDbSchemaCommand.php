426b27d755f2b4d4d10adae73fda0e98236dff2f
Edd Mann <the@eddmann.com>
2022-03-31 22:08:19 +0100

    Only allow DynamoDB schema command in dev/test

--- app/src/Ui/Cli/CreateDynamoDbSchemaCommand.php
+++ app/src/Ui/Cli/CreateDynamoDbSchemaCommand.php
@@ -15,7 +15,10 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Attribute\When;
 
+#[When(env: 'dev')]
+#[When(env: 'test')]
 final class CreateDynamoDbSchemaCommand extends Command
 {
     public function __construct(
