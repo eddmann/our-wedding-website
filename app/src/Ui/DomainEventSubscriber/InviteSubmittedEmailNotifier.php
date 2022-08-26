6ba2b1d63faba04045dc8993aab7125f90e1aeb3
Edd Mann <the@eddmann.com>
2022-08-26 13:55:25 +0100

    Move domain event subscribers to Ui layer

--- app/src/Ui/DomainEventSubscriber/InviteSubmittedEmailNotifier.php
+++ app/src/Ui/DomainEventSubscriber/InviteSubmittedEmailNotifier.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Infrastructure\DomainEventSubscribers;
+namespace App\Ui\DomainEventSubscriber;
 
 use App\Domain\Event\InviteSubmitted;
 use App\Domain\Helpers\DomainEventSubscriber;
